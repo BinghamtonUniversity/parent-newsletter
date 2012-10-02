690467a72d0e77eab42a9a30906a203f4cecd3a9
Adarsha <adarshakb@adarshakb-Lenovo-U410.(none)>
2012-10-02 12:14:49 -0400

    CK Editor added and integrated
    Now an integration for the CKEditor has been done.
    Next is to provide services and to actually do the things
    
    Signed-off-by:  <adarshakb>

--- headers.php
+++ headers.php
@@ -2,3 +2,4 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="description" content="Announcement tracker">
 <link rel="stylesheet" type="text/css" href="mos-css/mos-style.css"> <!--pemanggilan file css-->
+<script src="ckeditor/ckeditor.js" type="text/javascript"></script>
--- panel.php
+++ panel.php
@@ -12,49 +12,19 @@ include_once('base/include_top.php');
 <div id="wrapper">
 	<?php include_once('leftbar.php'); ?>
 	<div id="rightContent">
-	<h3>Admins</h3>
 	<?php if(isset($_GET['error'])) { ?>}
 	<div class="gagal">
 		 <?php echo $_GET['error']; ?>
 	</div>
 	<?php } ?>
-		<tbody>
-			<table class="data">
-				<tr class="data">
-					<th class="data" width="30px">No</th>
-					<th class="data" width="30px">Del</th>
-					<th class="data">Username</th>
-				</tr>
-				<?php $admins = Admin::AllAdmins(); $i=1;
-						foreach($admins as $a) {
-							?>
-				<tr class="data">
-					
-					<td class="data" width="30px"><?=$i;?></td>
-					<td class="data" width="30px"><a href="services/deleteAdmin.php?user=<?=urlencode($a->getUsername())?>">X</a></td>
-					<td class="data"><?=$a->getUsername();?></td>
-					
-				</tr>
 
-				<?php
-				}
-				?>
-			</table>
-		</tbody>
-		<div id="loginForm">
-	<div class="headLoginForm">
-	Add Administrator
-	</div>
-	<div class="fieldLogin">
-	<form method="POST" action="services/addAdmin.php">
-	<label>Username</label><br>
-	<input name="user" id="user" type="text" class="login" value="<?php if(isset($_GET['user'])) echo $_GET['user']; ?>"><br>
-	<label>Password</label><br>
-	<input name="passwd" id="passwd" type="password" class="login" value=""><br>
-	<input type="submit" class="button" value="Add Admin">
-	</form>
-	</div>
-</div>
+	<h3>Enter the new post:</h3>
+	<textarea class="ckeditor" name="editor1"></textarea>
+	<script type="text/javascript">
+		CKEDITOR.replace( 'editor1',{
+			height: '600px'
+		} );
+	</script>
 	</div>
 <?php include_once('footer.php'); ?>
 </div>
